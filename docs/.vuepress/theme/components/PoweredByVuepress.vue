0f5cb4f7459c26e1457117cc9cff7e7329d6b2af
Mike Wazovzky <mike.wazovzky@gmail.com>
2019-08-24 14:20:41 +0300

    Customize default theme

---
+++ docs/.vuepress/theme/components/PoweredByVuepress.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="powered-by">
+        <div>Powered by <a href="https://vuepress.vuejs.org/">Vuepress</a></div>
+        <img class="powered-by-img" src="/vuepress.png">
+    </div>
+</template>
+
+<script>
+export default {
+    //
+}
+</script>
+
+<style lang="stylus">
+.powered-by
+    text-align center
+    margin-top 20px
+.powered-by-img
+    width 140px
+    margin-top 20px
+</style>
+
+
